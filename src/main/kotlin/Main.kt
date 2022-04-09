625f7bc898181b7ba3d7e5c2d6ac69f8625223b4
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-04-09 18:59:14 +0700

    add: simple sample Rx Java

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,39 @@
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+
 
 fun main(args: Array<String>) {
+    Observable.create { emitter: ObservableEmitter<String> ->
+        try {
+            val nilai = "a".toInt()
+            emitter.onNext("Hello World")
+            emitter.onComplete()
+        } catch (e: Exception) {
+            emitter.onError(e)
+        }
+    }
+        .doOnSubscribe { println("showProgress") }
+        .doOnTerminate { println("hideProgress") }
+        .subscribe(object : Observer<String> {
 
+            override fun onSubscribe(d: Disposable) {
+                println("onSubscribe")
+                d.isDisposed
+            }
+
+            override fun onNext(t: String) {
+                println(t)
+            }
+
+            override fun onError(e: Throwable) {
+                println("onError")
+            }
+
+            override fun onComplete() {
+                println("onComplete")
+            }
+
+        })
 }
