d963131267dcfbb3aeae9686699ed18ce16b7c3c
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-04-11 11:35:54 +0700

    update: adding simple RX Java

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableEmitter
 import io.reactivex.rxjava3.core.Observer
@@ -5,9 +6,47 @@ import io.reactivex.rxjava3.disposables.Disposable
 
 
 fun main(args: Array<String>) {
+    completableFromAction()
+}
+
+fun completableFromAction() {
+    Completable.fromAction {
+        val nilai = "a".toInt()
+    }.doOnSubscribe { println("doOnSubscribe") }
+        .doOnTerminate { println("doOnTerminate") }
+        .subscribe({
+            println("Completed")
+        }) {
+            println("Error")
+        }
+}
+
+fun observableJust() {
+    Observable.just("Hello World")
+        .doOnSubscribe { println("showProgress") }
+        .doOnTerminate { println("hideProgress") }
+        .subscribe(object : Observer<String> {
+            override fun onComplete() {
+                println("Completed")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                println("onSubscribe")
+            }
+
+            override fun onNext(t: String) {
+                println("onNext $t")
+            }
+
+            override fun onError(e: Throwable) {
+                println("onError")
+            }
+        })
+}
+
+fun observableCreate() {
     Observable.create { emitter: ObservableEmitter<String> ->
         try {
-            val nilai = "a".toInt()
             emitter.onNext("Hello World")
             emitter.onComplete()
         } catch (e: Exception) {
@@ -17,23 +56,21 @@ fun main(args: Array<String>) {
         .doOnSubscribe { println("showProgress") }
         .doOnTerminate { println("hideProgress") }
         .subscribe(object : Observer<String> {
-
-            override fun onSubscribe(d: Disposable) {
-                println("onSubscribe")
-                d.isDisposed
-            }
-
-            override fun onNext(t: String) {
-                println(t)
-            }
-
-            override fun onError(e: Throwable) {
-                println("onError")
-            }
-
             override fun onComplete() {
                 println("onComplete")
             }
 
+            override fun onSubscribe(d: Disposable) {
+                println("onSubscribe")
+            }
+
+            override fun onNext(t: String) {
+                println("onNext $t")
+            }
+
+            override fun onError(e: Throwable) {
+                println("onError ${e.message}")
+
+            }
         })
 }
